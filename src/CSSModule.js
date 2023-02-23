dcb9c905d48eb48cee1bd8d28db2531637d08bcd
Juhee Noh <nohjuhee227@gmail.com>
2023-02-23 16:34:12 +0900

    [learn] 9.3.1 classnames

---
+++ src/CSSModule.js
@@ -0,0 +1,16 @@
+import classNames from "classnames";
+import styles from "./CSSModule.module.css";
+
+const cx = classNames.bind(styles);
+
+const CSSModule = () => {
+  // console.log(styles);
+  console.log(cx);
+  return (
+    <div className={cx("wrapper", "inverted")}>
+      안녕하세요, 저는 <span className="something">CSS Module!</span>
+    </div>
+  );
+};
+
+export default CSSModule;
